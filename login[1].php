284ac05492816a435b1693829a967d736ebcc6a1
lyrashakinahsalsabila <141093268+lyrashakinahsalsabila@users.noreply.github.com>
2023-08-05 12:58:40 +0700

    Add files via upload

---
+++ login[1].php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+     <title>LOGIN</title>
+         <link rel="stylesheet" href="style.css">
+ </head>
+ <body>
+    
+     <form action="" method="POST">
+         <h2>FORM LOGIN</h2>
+         
+            <br>
+                 <label for="username ">Username</label>
+                 <input type="text" name="username" id="username">
+             </br>
+             
+             <br>
+                 <label for="password ">Password</label>
+                 <input type="password" name="password" id="username">
+             
+             </br>
+        
+               <br>
+                 <button type="submit" name="login">LOGIN</button>
+                 </br>
+            
+         
+         
+         
+         
+     </body>
+     </form>
+     </html>
